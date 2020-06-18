b924c94ac71c931d5c0ae15df0917b6b1022606b
Jonathan Hsu <jonhsu.design@gmail.com>
2020-06-18 09:49:28 -0700

    added post method

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const Hobbits = require("../hobbits/hobbitsModel.js");
+const { json } = require("express");
 
 const server = express();
 
@@ -18,6 +19,14 @@ server.get("/hobbits", (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+server.post('/hobbits', (req,res) => {
+  Hobbits.insert(req.body)
+    .then(hobbit => {
+      res.status(201).json(hobbit);
+    })
+    .catch(err => res.status(500).json(err.message));
 });
 
 module.exports = server;
